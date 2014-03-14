8205bc50314469437205e6533e4478a3e3251ce9
Yankovsky <YankovskyAndrey@gmail.com>
2014-03-15 03:15:10 +0400

    finish slider

--- js/slider.js
+++ js/slider.js
@@ -1,76 +1,92 @@
-var slides = [
-        {src: 'images/slides/slide1.png', url: '/about'},
-        {src: 'images/slides/slide2.png', url: '/contacts'}
-    ],
-    slidesCount = slides.length,
-    uploadedSlidesCount = 0,
-    currentSlideNumber = 0
+(function($) {
+    $.fn.slider = function(slides, slideShowSpeed, animationSpeed) {
+        var that = this,
+            slidesCount = slides.length,
+            uploadedSlidesCount = 0,
+            currentSlideNumber,
+            timeout,
+            slideShowPaused = false,
+            $slides, $bullets, $details
 
-var delay = 2000
+        _.each(slides, function(slide) {
+            var img = new Image()
+            img.onload = function() {
+                uploadedSlidesCount++
+                if (uploadedSlidesCount >= slidesCount)
+                    init()
+            }
+            img.src = slide.src
+        })
 
-var timeout
-var $slider, $slides, $bullets, $details
-var mouseInsideSlider = false
+        function init() {
+            slideShowPaused = that.is(':hover')
+            that.mouseenter(function() {
+                slideShowPaused = true
+                clearSlideShowTimeout()
+            }).mouseleave(function() {
+                slideShowPaused = false
+                slideShow()
+            })
 
-_.each(slides, function(slide) {
-    var img = new Image()
-    img.onload = function() {
-        uploadedSlidesCount++
-        if (uploadedSlidesCount >= slidesCount) {
-            startSlider()
+            var $slidesContainer = that.find('.slides')
+            _.each(slides, function(slide) {
+                $slidesContainer.append('<img src="' + slide.src + '" alt="' + slide.alt + '">')
+            })
+            $slides = $slidesContainer.children()
+
+            var $bulletsContainer = that.find('.bullets')
+            _.times(slidesCount, function() {
+                $bulletsContainer.append('<a class="bullet" href="#"></a>')
+            })
+            $bullets = $bulletsContainer.children()
+            $bullets.click(function(e) {
+                e.preventDefault()
+                var index = $bullets.index(this)
+                if (index != currentSlideNumber)
+                    setSlide($bullets.index(this))
+            })
+
+            $details = that.find('.details')
+
+            setSlide(0)
+
+            that.find('.left-arrow').click(function(e) {
+                e.preventDefault()
+                nextSlide(-1)
+            })
+            that.find('.right-arrow').click(function(e) {
+                e.preventDefault()
+                nextSlide(1)
+            })
+        }
+
+        function clearSlideShowTimeout() {
+            if (timeout)
+                clearTimeout(timeout)
+        }
+
+        function slideShow() {
+            clearSlideShowTimeout()
+            timeout = setTimeout(function() {
+                nextSlide(1)
+            }, slideShowSpeed)
+        }
+
+        function setSlide(slideNumber) {
+            if (!slideShowPaused)
+                slideShow()
+            $slides.eq(currentSlideNumber).fadeOut(animationSpeed).css({zIndex: 0})
+            currentSlideNumber = slideNumber
+            $slides.eq(currentSlideNumber).css('zIndex', 51).fadeIn(animationSpeed).css('zIndex', 50)
+
+            $bullets.removeClass('current')
+            $bullets.eq(currentSlideNumber).addClass('current')
+
+            $details.attr('href', slides[currentSlideNumber].url)
+        }
+
+        function nextSlide(direction) {
+            setSlide((currentSlideNumber + direction + slidesCount) % slidesCount)
         }
     }
-    img.src = slide.src
-})
-
-function startSlider() {
-    $slider = $('#slider')
-
-    var $slidesContainer = $slider.find('.slides');
-    _.each(slides, function(slide) {
-        $slidesContainer.append('<img src="' + slide.src + '">')
-    })
-
-    var $bulletsContainer = $slider.find('.bullets');
-    _.times(slidesCount, function() {
-        $bulletsContainer.append('<a class="bullet" href="#"></a>')
-    })
-
-    $slides = $slidesContainer.children()
-    $bullets = $bulletsContainer.children()
-    $details = $slider.find('.details')
-
-    changeSlide()
-
-
-    $slider.mouseenter(function() {
-        mouseInsideSlider = true
-    }).mouseleave(function() {
-        mouseInsideSlider = false
-    })
-    $('.left-arrow').click(function() {
-        clearTimeout(timeout)
-        extracted(-1)
-    })
-    $('.right-arrow').click(function() {
-        clearTimeout(timeout)
-        extracted(1)
-    })
-}
-
-function changeSlide() {
-    $slides.removeClass('current')
-    $slides.eq(currentSlideNumber).addClass('current')
-    $bullets.removeClass('current')
-    $bullets.eq(currentSlideNumber).addClass('current')
-    $details.attr('href', slides[currentSlideNumber].url)
-
-    timeout = setTimeout(function() {
-        extracted(1)
-    }, delay)
-}
-
-function extracted(direction) {
-    currentSlideNumber = (currentSlideNumber + direction + slidesCount) % slidesCount
-    changeSlide()
-}
+}(jQuery))
